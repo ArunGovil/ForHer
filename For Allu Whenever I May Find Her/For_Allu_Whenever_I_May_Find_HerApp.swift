61f30beb5b87cdb152dfe9ba0f9b4b741e1eeb59
ArunGovil <arun.govil@unifize.com>
2025-06-30 21:59:41 +0530

    Initial Commit

---
+++ For Allu Whenever I May Find Her/For_Allu_Whenever_I_May_Find_HerApp.swift
@@ -0,0 +1,17 @@
+//
+//  For_Allu_Whenever_I_May_Find_HerApp.swift
+//  For Allu Whenever I May Find Her
+//
+//  Created by Arun on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct For_Allu_Whenever_I_May_Find_HerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
